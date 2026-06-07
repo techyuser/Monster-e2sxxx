456befc2939d255a5a6690f52019f83484b7d3e5
nv2500 <1527411+nv2500@users.noreply.github.com>
2026-06-06 22:34:14 -0600

    Update customize.sh

--- platform/exynos2100/patches/__desixtification/customize.sh
+++ platform/exynos2100/patches/__desixtification/customize.sh
@@ -5,6 +5,8 @@
     system/apex/com.android.i18n.apex
     system/apex/com.android.runtime.apex
     system/apex/com.google.android.tzdata6.apex
+    system/bin/bootstrap/linker
+    system/bin/bootstrap/linker_asan
     "
     for blob in $BLOBS_LIST
     do
@@ -13,10 +15,10 @@
     LOG_STEP_OUT
 
     LOG_STEP_IN "- Creating symlinks"
-    # ln -sf "/apex/com.android.runtime/bin/linker" "$WORK_DIR/system/system/bin/linker"
-    # ln -sf "/apex/com.android.runtime/bin/linker" "$WORK_DIR/system/system/bin/linker_asan"
-    # SET_METADATA "system" "system/bin/linker" 0 0 755 "u:object_r:system_file:s0"
-    # SET_METADATA "system" "system/bin/linker_asan" 0 0 755 "u:object_r:system_file:s0"
+    ln -sf "/apex/com.android.runtime/bin/linker" "$WORK_DIR/system/system/bin/linker"
+    ln -sf "/apex/com.android.runtime/bin/linker" "$WORK_DIR/system/system/bin/linker_asan"
+    SET_METADATA "system" "system/bin/linker" 0 0 755 "u:object_r:system_file:s0"
+    SET_METADATA "system" "system/bin/linker_asan" 0 0 755 "u:object_r:system_file:s0"
 
     ln -sf "/apex/com.android.runtime/lib/bionic/libc.so" "$WORK_DIR/system/system/lib/libc.so"
     ln -sf "/apex/com.android.runtime/lib/bionic/libdl.so" "$WORK_DIR/system/system/lib/libdl.so"
