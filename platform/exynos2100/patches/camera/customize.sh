56ed878f80259f3265a87522b3c516a6e99e2980
nv2500 <vothanhnhat85@gmail.com>
2026-06-08 22:12:54 -0600

    3.6.3 - update unica settings

--- platform/exynos2100/patches/camera/customize.sh
+++ platform/exynos2100/patches/camera/customize.sh
@@ -103,13 +103,13 @@
 # do
 #     ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "$blob" 0 0 644 "u:object_r:system_lib_file:s0" &
 # done
-
-LOG_STEP_IN "- Fixing vendor display props"
-# DPI
-LCD_DENSITY="$(GET_PROP "vendor" "ro.sf.lcd_density")"
-if [ "$LCD_DENSITY" ]; then
-    SET_PROP "vendor" "ro.sf.init.lcd_density" "$LCD_DENSITY"
-else
-    ABORT "ro.sf.lcd_density prop not found in vendor"
-fi
-LOG_STEP_OUT
+# 
+# LOG_STEP_IN "- Fixing vendor display props"
+# # DPI
+# LCD_DENSITY="$(GET_PROP "vendor" "ro.sf.lcd_density")"
+# if [ "$LCD_DENSITY" ]; then
+#     SET_PROP "vendor" "ro.sf.init.lcd_density" "$LCD_DENSITY"
+# else
+#     ABORT "ro.sf.lcd_density prop not found in vendor"
+# fi
+# LOG_STEP_OUT
--- unica/configs/version.sh
+++ unica/configs/version.sh
@@ -4,7 +4,7 @@
 # Only the below variable(s) need to be changed!
 VERSION_MAJOR=3
 VERSION_MINOR=6
-VERSION_PATCH=2
+VERSION_PATCH=4
 
 # The below variables will be generated automatically
 #
